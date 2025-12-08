65c9d250b34e22ba6c282a57e8f24672cd2b5cde
lashu <lashurov44@gmail.com>
2025-12-08 16:42:35 +0200

    hw eduard

--- src/repository/studentRepository.js
+++ src/repository/studentRepository.js
@@ -1,62 +1,64 @@
 import { Student } from "../module/student.js";
 
-
 const students = new Map();
 
-
 export const addStudent = ({id, name, password}) => {
-    if(students.has(id)) {
+    id = +id; // דואג שהוא מספר
+
+    if (students.has(id)) {
         return false;
     }
     students.set(id, new Student(id, name, password));
     return true;
-}
+};
 
+export const findStudent = (id) => {
+    id = +id;
+    return students.get(id);
+};
 
-export const findStudent = id => students.get(id);
-
-
-export const deleteStudent = id => {
+export const deleteStudent = (id) => {
+    id = +id;
     const student = students.get(id);
     if (student) {
         students.delete(id);
-        return student;
+        return student; // מחזיר את הסטודנט שנמחק
     }
-}
-
+    return null;
+};
 
 export const updateStudent = (id, data) => {
+    id = +id;
     const student = students.get(id);
     if (student) {
         Object.assign(student, data);
         return student;
     }
-}
-
+    return null;
+};
 
 export const addScore = (id, exam, score) => {
+    id = +id;
     const student = students.get(id);
     if (student) {
         student.scores[exam] = score;
         return true;
-
-
     }
     return false;
-}
-
+};
 
 export const findByName = (name) => {
-    return Array.from(students.values()).filter(s => s.name.toLowerCase() === name.toLowerCase());
-}
-
+    return Array.from(students.values())
+        .filter(s => s.name.toLowerCase() === name.toLowerCase());
+};
 
 export const countByNames = (names) => {
     names = names.map(name => name.toLowerCase());
-    return Array.from(students.values()).filter(s => names.includes(s.name.toLowerCase())).length;
-}
-
+    return Array.from(students.values())
+        .filter(s => names.includes(s.name.toLowerCase())).length;
+};
 
 export const findByMinScore = (exam, minScore) => {
-    return Array.from(students.values()).filter(s => s.scores[exam] >= minScore);
-}
+    return Array.from(students.values())
+        .filter(s => s.scores[exam] >= minScore);
+};
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -12,7 +12,7 @@ const router = Router();
 
 router.post("/student", addStudent);
 router.get("/student/:id", findStudent);
-router.delete("student/:id", deleteStudent);
+router.delete("/student/:id", deleteStudent);
 router.patch("/student/:id", updateStudent);
 router.patch("/score/student/:id", addScore);
 router.get("/student/name/:name", findByName);
